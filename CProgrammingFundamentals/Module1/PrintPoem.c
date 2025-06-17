89cf4da4444a505b404ed030891a784ad8ddb4ef
RDiegoFG <rubenfg.contact@gmail.com>
2025-06-17 11:25:00 -0700

    Make poem more readable

--- CProgrammingFundamentals/Module1/PrintPoem.c
+++ CProgrammingFundamentals/Module1/PrintPoem.c
@@ -1,8 +1,18 @@
 //Use the printf() statement to output a poem of your choice. Submit your program as a text file.
 //Change 1 - Add poem
+//Change 2 - Make peom more readable
 #include<stdio.h>
 int main(void)
 {   
-    printf("Caminante, son tus huellas\nel camino y nada más;\nCaminante, no hay camino,\nse hace camino al andar.\nAl andar se hace el camino,\ny al volver la vista atrás\nse ve la senda que nunca\nse ha de volver a pisar.\nCaminante no hay camino\nsino estelas en la mar.");
+    printf("Caminante, son tus huellas\n");
+    printf("el camino y nada más;\n");
+    printf("Caminante, no hay camino,\n");
+    printf("se hace camino al andar.\n");
+    printf("Al andar se hace el camino,\n");
+    printf("y al volver la vista atrás\n");
+    printf("se ve la senda que nunca\n");
+    printf("se ha de volver a pisar.\n");
+    printf("Caminante no hay camino\n");
+    printf("sino estelas en la mar.\n");
     return 0;
 }    
