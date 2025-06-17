539bf96e03b866ee23dc01171eae70eee0c9a437
RDiegoFG <rubenfg.contact@gmail.com>
2025-06-17 11:12:18 -0700

    Add poem to PrintPoem.c

--- CProgrammingFundamentals/Module1/PrintPoem.c
+++ CProgrammingFundamentals/Module1/PrintPoem.c
@@ -1,6 +1,8 @@
-//Use the printf() statement to output a poem of your choice. Submit your program as a text file. 
+//Use the printf() statement to output a poem of your choice. Submit your program as a text file.
+//Change 1 - Add poem
 #include<stdio.h>
 int main(void)
 {   
+    printf("Caminante, son tus huellas\nel camino y nada más;\nCaminante, no hay camino,\nse hace camino al andar.\nAl andar se hace el camino,\ny al volver la vista atrás\nse ve la senda que nunca\nse ha de volver a pisar.\nCaminante no hay camino\nsino estelas en la mar.");
     return 0;
 }    
