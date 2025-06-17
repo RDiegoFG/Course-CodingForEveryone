7664ef47c7d476d25066d2fab217a337a1b11a21
RDiegoFG <rubenfg.contact@gmail.com>
2025-06-17 11:07:31 -0700

    Fix code for submission.

--- CProgrammingFundamentals/Module1/FixMistake.c
+++ CProgrammingFundamentals/Module1/FixMistake.c
@@ -1,7 +1,8 @@
 //The following program is suppose to write hello world onto the screen but it has syntax errors  - find and correct.
+//Change 1 - changed ' to "
 #include<stdio.h>
 int main(void)
 {   
-    printf(‘ hello world\n’);
+    printf("hello world\n");
     return 0;
 }    
